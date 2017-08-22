3a8c278a808af6fd0867c79398bf617ff6c932e9
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2017-08-22 14:52:46 +0100

    accordian

--- net-build/push.ps1
+++ net-build/push.ps1
@@ -7,6 +7,9 @@ if (! (Test-Path Env:\APPVEYOR_REPO_TAG_NAME)) {
 
 Write-Host Starting push stage - net-build...
 
+# invoke the build for container re-build
+./build.ps1
+
 # log on to docker
 docker login $env:DOCKER_REGISTRY -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
 
