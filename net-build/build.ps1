b64b8537885d3330190d5b4fb5dd883eba961ad6
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2017-08-29 16:48:33 +0100

    why do build failures not get regsitered

--- net-build/build.ps1
+++ net-build/build.ps1
@@ -8,7 +8,7 @@ docker pull microsoft/windowsservercore
 # build base image
 docker build -t $env:DOCKER_REGISTRY/net-build -f net-build/Dockerfile .
 
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+
 # image list
 docker images
-
-if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
